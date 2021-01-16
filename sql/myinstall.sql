d80764eabeeb8abdfd82f2e48818f1f52496d5bc
lalgwebdev <webtech@lalg.org.uk>
2021-01-16 16:49:54 +0200

    Initial selection from lalgutils - Untested

---
+++ sql/myinstall.sql
@@ -0,0 +1,7 @@
+INSERT INTO civirule_action (name, label, class_name, is_active) 
+VALUES("lalg_update_hh_mship_info", "LALG Update HH Mship Info", "CRM_Civirules_LalgUpdateHHMshipInfo", 1);
+INSERT INTO civirule_action (name, label, class_name, is_active) 
+VALUES("lalg_tidy_billing_address", "LALG Tidy Billing Address", "CRM_Civirules_LalgTidyBillingAddress", 1);
+INSERT INTO civirule_action (name, label, class_name, is_active) 
+VALUES("lalg_display_triggerdata", "LALG Display TriggerData", "CRM_Civirules_LalgDisplayTriggerData", 1);
+
